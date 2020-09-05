3e8aa398d6655ce49d5f0286c12e5b111e17bc1f
James Mishra <james.mishra@neocrym.com>
2020-09-05 04:00:46 +0000

    Use more precise definitions for the AWS Batch module configuration

--- batch.tf
+++ batch.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "batch__execution_role" {
-  count = length(var.batch) > 0 ? 1 : 0
-  name  = "P-v1---${var.provose_config.name}---batch-ex-e-t-e-r"
+  for_each = var.batch
+  name     = "P-v1---${var.provose_config.name}---${each.key}-b-e-t-e-r"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -18,8 +18,8 @@ resource "aws_iam_role" "batch__execution_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "batch__execution_role" {
-  count      = length(var.batch) > 0 ? 1 : 0
-  role       = aws_iam_role.batch__execution_role[count.index].name
+  for_each   = aws_iam_role.batch__execution_role
+  role       = each.value.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
@@ -27,9 +27,9 @@ module "aws_iam_instance_profile__batch__execution_role" {
   source      = "./modules/iam_instance_profiles"
   aws_command = local.AWS_COMMAND
   profile_configs = {
-    for role in aws_iam_role.batch__execution_role :
-    "main" => {
-      instance_profile_name = "P-v1---${var.provose_config.name}---batch-ex-i-p"
+    for key, role in aws_iam_role.batch__execution_role :
+    (key) => {
+      instance_profile_name = "P-v1---${var.provose_config.name}---${key}---ex-i-p"
       path                  = "/"
       role_name             = role.name
     }
@@ -37,8 +37,8 @@ module "aws_iam_instance_profile__batch__execution_role" {
 }
 
 resource "aws_iam_role" "batch__service_role" {
-  count = length(var.batch) > 0 ? 1 : 0
-  name  = "P-v1---${var.provose_config.name}---batch-s-r"
+  for_each = var.batch
+  name     = "P-v1---${var.provose_config.name}---${each.key}-b-s-r"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -56,14 +56,14 @@ resource "aws_iam_role" "batch__service_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "batch__service_role" {
-  count      = length(var.batch) > 0 ? 1 : 0
-  role       = aws_iam_role.batch__service_role[count.index].name
+  for_each   = aws_iam_role.batch__service_role
+  role       = each.value.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
 resource "aws_iam_role" "batch__spot_fleet_role" {
-  count = length(var.batch) > 0 ? 1 : 0
-  name  = "P-v1---${var.provose_config.name}---batch-spot-fleet-r"
+  for_each = var.batch
+  name     = "P-v1---${var.provose_config.name}---${each.key}-b-spot-fleet-r"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -81,8 +81,8 @@ resource "aws_iam_role" "batch__spot_fleet_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "batch__spot_fleet_role" {
-  count      = length(var.batch) > 0 ? 1 : 0
-  role       = aws_iam_role.batch__spot_fleet_role[count.index].name
+  for_each   = aws_iam_role.batch__spot_fleet_role
+  role       = each.value.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole"
 }
 
@@ -155,16 +155,15 @@ resource "aws_security_group" "batch" {
 resource "aws_batch_compute_environment" "batch" {
   for_each = var.batch
 
-  compute_environment_name = each.key
-
   compute_resources {
-    instance_role       = module.aws_iam_instance_profile__batch__execution_role.instance_profiles["main"].arn
-    spot_iam_fleet_role = aws_iam_role.batch__spot_fleet_role[0].arn
-    bid_percentage      = 100
-    instance_type       = each.value.instances.instance_types
-    type                = each.value.instances.compute_environment_type
-    max_vcpus           = each.value.instances.max_vcpus
-    min_vcpus           = each.value.instances.min_vcpus
+    instance_role       = module.aws_iam_instance_profile__batch__execution_role.instance_profiles[each.key].arn
+    spot_iam_fleet_role = aws_iam_role.batch__spot_fleet_role[each.key].arn
+    # TODO: Let the user set the bid percentage for AWS Batch compute environments with Spot Instances.
+    bid_percentage = 100
+    instance_type  = each.value.instances.instance_types
+    type           = each.value.instances.compute_environment_type
+    max_vcpus      = each.value.instances.max_vcpus
+    min_vcpus      = each.value.instances.min_vcpus
 
     security_group_ids = [aws_security_group.batch[each.key].id]
     subnets            = aws_subnet.vpc[*].id
@@ -174,25 +173,36 @@ resource "aws_batch_compute_environment" "batch" {
     }
   }
 
-  service_role = aws_iam_role.batch__service_role[0].arn
+  service_role = aws_iam_role.batch__service_role[each.key].arn
   type         = "MANAGED"
   depends_on = [
     aws_iam_role_policy_attachment.batch__service_role,
     aws_security_group.batch
   ]
+
+  compute_environment_name_prefix = each.key
+
+  lifecycle {
+    # We need aws_batch_compute_environment resources to be named with a name_prefix
+    # and for the create_before_destroy flag to be true. Otherwise, Terraform will
+    # run into an error because it tries to delete the compute environment without
+    # first deleting the associated queues.
+    # For more information, see: https://github.com/terraform-providers/terraform-provider-aws/issues/2044
+    create_before_destroy = true
+  }
 }
 
 resource "aws_batch_job_queue" "batch" {
   for_each = zipmap(
     flatten([
       for compute_environment_name, compute_environment_config in var.batch : [
-        for queue_name, queue_config in try(var.batch[compute_environment_name].queues, []) :
+        for queue_name, queue_config in try(var.batch[compute_environment_name].job_queues, []) :
         join("-", [compute_environment_name, "queue", queue_name])
       ]
     ]),
     flatten([
       for compute_environment_name, compute_environment_config in var.batch : [
-        for queue_name, queue_config in try(var.batch[compute_environment_name].queues, []) :
+        for queue_name, queue_config in try(var.batch[compute_environment_name].job_queues, []) :
         merge(
           {
             compute_environment_name = compute_environment_name
@@ -215,13 +225,13 @@ resource "aws_batch_job_definition" "batch" {
   for_each = zipmap(
     flatten([
       for compute_environment_name, compute_environment_config in var.batch : [
-        for job_name, job_config in try(var.batch[compute_environment_name].jobs, []) :
+        for job_name, job_config in try(var.batch[compute_environment_name].job_definitions, []) :
         join("-", [compute_environment_name, "job", job_name])
       ]
     ]),
     flatten([
       for compute_environment_name, compute_environment_config in var.batch : [
-        for job_name, job_config in try(var.batch[compute_environment_name].jobs, []) :
+        for job_name, job_config in try(var.batch[compute_environment_name].job_definitions, []) :
         merge(
           {
             compute_environment_name = compute_environment_name
