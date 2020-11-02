41773c341c38e14c1bba10a504da6c9eb178f29b
James Mishra <james.mishra@neocrym.com>
2020-11-02 05:40:03 -0600

    Pull documentation from main Provose repo

--- clone-docs-dir.sh
+++ clone-docs-dir.sh
@@ -2,7 +2,7 @@
 # This script ONLY clones the docs directory for the Provose repo.
 set -Eeuxo pipefail
 rm -rf ./cache ./$1
-git clone --quiet --single-branch --branch $1.x --quiet --depth 1 --filter=blob:none --no-checkout https://github.com/provose/provose-docs-improvement cache
+git clone --quiet --single-branch --branch $1.x --quiet --depth 1 --filter=blob:none --no-checkout https://github.com/provose/provose cache
 cd cache
 git checkout $1.x -- docs 2> /dev/null
 cd ..
