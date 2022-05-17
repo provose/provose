2b4f81488fd23cfec8a4293026d0ab49fbca98f9
James Mishra <james.mishra@neocrym.com>
2022-05-17 15:30:26 -0500

    Do not create an ECR image repository policy when there are no principals
    Before this fix, if we tried to create an ECR repository
    **without** any ECS or ECR resources as the "principal"
    to consume the policy, the policy would be invalid JSON and
    `terraform apply` would fail.

--- images.tf
+++ images.tf
@@ -18,20 +18,44 @@ EOF
   }
 }
 
-resource "aws_ecr_repository_policy" "images" {
-  for_each   = aws_ecr_repository.images
-  repository = each.value.name
+
+resource "aws_ecr_repository_policy" "images_ecs" {
+  for_each = merge([
+    for image_key, image in aws_ecr_repository.images : {
+      for role_key, role in aws_iam_role.iam__ecs_task_execution_role :
+      "${image_key}---${role_key}" => {
+        repository = image.name
+        principal  = jsonencode(role.arn)
+      }
+    }
+  ]...)
+
+  repository = each.value.repository
   policy = templatefile(
     "${path.module}/templates/ecr_repository_policy.json",
     {
-      principal = jsonencode(concat(
-        [
-          for role in aws_iam_role.iam__ecs_task_execution_role : role.arn
-        ],
-        [
-          for role in aws_iam_role.ec2_on_demand_instances : role.arn
-        ]
-      ))
+      principal = each.value.principal
+    }
+  )
+}
+
+
+resource "aws_ecr_repository_policy" "images_ec2_on_demand_instances" {
+  for_each = merge([
+    for image_key, image in aws_ecr_repository.images : {
+      for role_key, role in aws_iam_role.ec2_on_demand_instances :
+      "${image_key}---${role_key}" => {
+        repository = image.name
+        principal  = jsonencode(role.arn)
+      }
+    }
+  ]...)
+
+  repository = each.value.repository
+  policy = templatefile(
+    "${path.module}/templates/ecr_repository_policy.json",
+    {
+      principal = each.value.principal
     }
   )
 }
@@ -44,7 +68,8 @@ output "images" {
       images = aws_ecr_repository.images
     }
     aws_ecr_repository_policy = {
-      images = aws_ecr_repository_policy.images
+      images_ecs = aws_ecr_repository_policy.images_ecs
+      images_ec2_on_demand_instances = aws_ecr_repository_policy.images_ec2_on_demand_instances
     }
   }
 }
