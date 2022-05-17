d289b789a5bf14b97c51400d28fb8f46b829eee6
James Mishra <james.mishra@neocrym.com>
2022-05-16 22:32:45 -0500

    Add docs for v4.0

--- build-all-versions.sh
+++ build-all-versions.sh
@@ -9,10 +9,12 @@ rm -rfv docs _site
 ./clone-docs-dir.sh v1.1
 ./clone-docs-dir.sh v2.0
 ./clone-docs-dir.sh v3.0
+./clone-docs-dir.sh v4.0
 ./fix-titles.sh v1.0 "v1.0 (Deprecated)"
 ./fix-titles.sh v1.1 "v1.1 (Deprecated)"
 ./fix-titles.sh v2.0 "v2.0 (Deprecated)"
 ./fix-titles.sh v3.0 "v3.0 (Stable)"
+./fix-titles.sh v4.0 "v4.0 (Alpha Preview)"
 jekyll build
 touch docs/.nojekyll
 cp CNAME docs
